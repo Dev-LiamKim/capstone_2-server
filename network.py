5863c9ebc552ce0841cf5f65e431d26053b11320
unknown <LiamKim>
2026-05-30 14:31:04 +0900

    network recieve structure modified

--- network.py
+++ network.py
@@ -1,4 +1,3 @@
-# network.py
 import socket
 import struct
 import numpy as np
@@ -11,6 +10,7 @@ class EMGReceiver:
         self.sock.bind(('0.0.0.0', port))
         self.sock.listen(1)
         self.conn = None
+        self.buffer = b''  # 데이터 보존용 누적 버퍼 추가
 
     def wait_for_connection(self):
         print("ESP32 접속 대기 중...")
@@ -20,13 +20,20 @@ class EMGReceiver:
 
     def receive_batch(self):
         try:
-            data = b''
-            while len(data) < PACKET_SIZE:
-                packet = self.conn.recv(PACKET_SIZE - len(data))
-                if not packet: return None
-                data += packet
+            # 논블로킹으로 수신 가능한 모든 데이터 누적
+            packet = self.conn.recv(4096)
+            if not packet: 
+                return None
+            self.buffer += packet
+        except BlockingIOError:
+            pass  # 현재 수신 큐가 비어있을 경우 예외 무시 후 버퍼 길이 확인 단계로 이동
+
+        # 버퍼에 하나의 완전한 패킷 이상이 모였을 때만 파싱 처리
+        if len(self.buffer) >= PACKET_SIZE:
+            data = self.buffer[:PACKET_SIZE]
+            self.buffer = self.buffer[PACKET_SIZE:]  # 처리 완료된 데이터는 버퍼에서 제거
             
             unpacked = struct.unpack(f'<{BATCH_SIZE * CHANNELS}i', data)
             return np.array(unpacked).reshape(BATCH_SIZE, CHANNELS).T
-        except BlockingIOError:
-            return None
+            
+        return None
